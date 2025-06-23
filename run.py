78221e2afeca3e29fac175a0e89a56e2117190a7
jomaguevco <gonfrecss2003@icloud.com>
2025-06-23 07:59:45 -0500

    Fix stray prompt text and tidy run script

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from app import create_app #, socketio
-
+from app import create_app  # , socketio
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #socketio.run(app, host='0.0.0.0', port=8000, debug=True)
+    # socketio.run(app, host='0.0.0.0', port=8000, debug=True)
